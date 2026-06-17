7080ac9cac9f3caa937d2a3d68d4d98b614ba9e3
Tristan <tristan.reig@gmail.com>
2026-06-17 21:53:03 +0200

    chore: clean dependencies and fix npm audit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/time" : {
-        target: "http://localhost:5000/",
-        secure: false
-      },
-    },
-  },
   plugins: [react()]
 })
